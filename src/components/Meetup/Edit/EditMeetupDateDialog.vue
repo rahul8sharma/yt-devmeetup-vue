28b4639a4f2d336c927190fe90d174b0f71592cb
Rahul Sharma <rahul.sharma@jombay.com>
2018-04-02 18:20:28 +0530

    Modified "formatDate" method in "Meetup/Edit/EditMeetupDateDialog.vue" file.

--- src/components/Meetup/Edit/EditMeetupDateDialog.vue
+++ src/components/Meetup/Edit/EditMeetupDateDialog.vue
@@ -61,11 +61,11 @@
     }
   }
 
-  function formatDate(date) {
-    var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
+  function formatDate (date) {
+    var d = new Date(date)
+    var month = '' + (d.getMonth() + 1)
+    var day = '' + d.getDate()
+    var year = d.getFullYear()
 
     if (month.length < 2) month = '0' + month
     if (day.length < 2) day = '0' + day
